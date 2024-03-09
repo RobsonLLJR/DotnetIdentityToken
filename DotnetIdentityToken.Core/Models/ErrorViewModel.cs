e52c2011f0ae5b69f2e65543a5cd89c23a12c317
Robson Liesner de Lima Junior <robsonlljr@gmail.com>
2024-03-09 15:02:04 -0300

    Adicionar arquivos de projeto.

---
+++ DotnetIdentityToken.Core/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotnetIdentityToken.Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
